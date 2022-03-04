f03f51215ed0f345befc6a8cd64af49cc6b5fe29
Liam <78104591+Liam-Nothing@users.noreply.github.com>
2022-03-04 15:20:48 +0100

    Update

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<link rel="stylesheet" type="text/css" href="css/styles.css">
+		
+	</head>
+	<body>
+		<header>
+		
+		</header>
+		<main class="flex-container">
+			<img src="https://c.tenor.com/x8v1oNUOmg4AAAAd/rickroll-roll.gif"/>
+			<div id="audio"></div>
+		</main>
+		<footer>
+		
+		</footer>
+		<script src="js/func.js"></script>
+	</body>
+</html>
